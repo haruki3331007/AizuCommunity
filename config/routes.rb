f6a3a4bf4a909ced2c34530e00456dd239cac936
haruki3331007 <116860864+haruki3331007@users.noreply.github.com>
2023-10-26 00:00:16 +0200

    add devise, admin

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
+  devise_for :admin, skip: [:registrations, :passwords], controllers: {sessions: 'admin/sessions'}
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root "homes/top"
-  get "homes/about"
-  resources :news, only:[:index]
-  resources :posts
-  resources :user
-  resources :admin
-  resources :contacts, only: [:new, :create]
+  #root "homes/top"
+  #get "homes/about"
+  #resources :news, only:[:index]
+  #resources :posts
+  #resources :user
+  #resources :admin
+  #resources :contacts, only: [:new, :create]
 end
