90f01acdc6354ff6e152b5988e4adb002e3923ce
haruki3331007 <116860864+haruki3331007@users.noreply.github.com>
2023-10-25 23:38:21 +0200

    add routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root "homes/top"
+  get "homes/about"
+  resources: "news", only:[:index]
+  resources: "posts"
+  resources: "user"
+  resources: "admin", only:[:]
+  resources: "contacts", only: [:new, :create]
 end
