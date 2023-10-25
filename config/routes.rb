8fac2ce179f67d62e6071b3d8a5d51d289010f3e
haruki3331007 <116860864+haruki3331007@users.noreply.github.com>
2023-10-25 23:45:56 +0200

    fix routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,9 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root "homes/top"
   get "homes/about"
-  resources: "news", only:[:index]
-  resources: "posts"
-  resources: "user"
-  resources: "admin", only:[:]
-  resources: "contacts", only: [:new, :create]
+  resources :news, only:[:index]
+  resources :posts
+  resources :user
+  resources :admin
+  resources :contacts, only: [:new, :create]
 end
