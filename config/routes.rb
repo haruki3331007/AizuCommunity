9f3f0ed1b8910763e7feb13207bdc564cfc2fa36
haruki3331007 <116860864+haruki3331007@users.noreply.github.com>
2023-10-26 12:10:37 +0200

    add users controller, routes, view

--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,13 @@ Rails.application.routes.draw do
   devise_for :admin, skip: [:registrations, :passwords], controllers: {sessions: 'admin/sessions'}
   devise_for :users, skip: [:passwords], controllers:{registrations: "user/registrations", 
                                                       sessions:      'user/sessions'}
-  
   root to: 'homes#top'
   get 'aboutUs' => 'homes#aboutUs'
   resources :news, only:[:index]
-  resources :posts
-  #resources :user
-  #resources :admin
   resources :contacts
+
+  resources :posts
+
+  resources :users, skip: [:new, :create]
+  #resources :admin
 end
