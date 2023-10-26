42f3e787b51783f28c5f11226491fa30c7378cf9
haruki3331007 <116860864+haruki3331007@users.noreply.github.com>
2023-10-26 11:47:50 +0200

    migrate devise User

--- db/migrate/20231026094020_devise_create_users.rb
+++ db/migrate/20231026094020_devise_create_users.rb
@@ -32,6 +32,9 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.1]
       # t.string   :unlock_token # Only if unlock strategy is :email or :both
       # t.datetime :locked_at
 
+      t.string :name, null: false
+      t.string :user_name, null: false, unique: true
+      t.integer :location, null: true
 
       t.timestamps null: false
     end
