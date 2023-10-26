3ae7da8ff7ff819c9fb9f94e5ed080f5fa4b05db
haruki3331007 <116860864+haruki3331007@users.noreply.github.com>
2023-10-26 11:09:49 +0200

    add homes controller, routes, view

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   devise_for :admin, skip: [:registrations, :passwords], controllers: {sessions: 'admin/sessions'}
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  #root "homes/top"
-  #get "homes/about"
+  root to: 'homes#top'
+  get 'aboutUs' => 'homes#aboutUs'
   #resources :news, only:[:index]
   #resources :posts
   #resources :user
