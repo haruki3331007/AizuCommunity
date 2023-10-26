e6efc44b4ccf329dfe20d33c7714942fc7cde585
haruki3331007 <116860864+haruki3331007@users.noreply.github.com>
2023-10-26 18:51:52 +0200

    modify user registration

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,16 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  def town_name
+    case self.location
+      when 1
+          "会津若松市内"
+      when 2
+          "会津若松市外（福島県内）"
+      when 3
+          "会津若松市外（福島県外）"
+    end
+  end
+
 end
