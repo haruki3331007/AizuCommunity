5a29badf18edf743066f384d6db09f9cd6c06087
haruki3331007 <116860864+haruki3331007@users.noreply.github.com>
2023-10-27 18:00:05 +0200

    modify user show to show the account info.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,4 +2,22 @@ class UsersController < ApplicationController
     def show
         
     end
+
+    def edit
+
+    end
+
+    def update
+
+    end
+
+    def destroy
+
+    end
+
+    private
+
+    def user_params
+        
+    end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -8,4 +8,15 @@ class User < ApplicationRecord
   has_many :comments
   has_many :likes
 
+  def location_name
+    case self.location
+    when 1
+      "会津若松市内"
+    when 2
+      "会津若松市外（福島県内）"
+    else
+      '会津若松市外（福島県外）'
+    end
+  end
+
 end
