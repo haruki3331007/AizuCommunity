a862c7aca1ba983031c46100fc057ab9b6948ece
haruki3331007 <116860864+haruki3331007@users.noreply.github.com>
2023-10-27 10:50:03 +0200

    modify user registration

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -9,7 +9,7 @@ class PostsController < ApplicationController
 
     def create
         post = Post.new(post_params)
-        post.user_id = 1 #current_user.id
+        post.user_id = current_user.id
         if post.save
             redirect_to posts_path
         else
--- app/controllers/user/registrations_controller.rb
+++ app/controllers/user/registrations_controller.rb
@@ -1,18 +1,18 @@
 # frozen_string_literal: true
 
 class User::RegistrationsController < Devise::RegistrationsController
-  # before_action :configure_sign_up_params, only: [:create]
+  before_action :configure_sign_up_params, only: [:create]
   # before_action :configure_account_update_params, only: [:update]
 
   # GET /resource/sign_up
-  # def new
-  #   super
-  # end
+  #def new
+  #  super
+  #end
 
   # POST /resource
-  # def create
-  #   super
-  # end
+  #def create
+  #  super
+  #end
 
   # GET /resource/edit
   # def edit
@@ -38,12 +38,13 @@ class User::RegistrationsController < Devise::RegistrationsController
   #   super
   # end
 
-  # protected
+  protected
 
   # If you have extra params to permit, append them to the sanitizer.
-  # def configure_sign_up_params
-  #   devise_parameter_sanitizer.permit(:sign_up, keys: [:attribute])
-  # end
+  def configure_sign_up_params
+    info =  [:name, :user_name, :email, :location, :password, :password_confirmation]
+    devise_parameter_sanitizer.permit(:sign_up, keys: info)
+  end
 
   # If you have extra params to permit, append them to the sanitizer.
   # def configure_account_update_params
