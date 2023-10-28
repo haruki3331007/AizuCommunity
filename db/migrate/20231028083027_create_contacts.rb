336ed285d829177ab989727bf7f9a34ec013ae09
haruki3331007 <116860864+haruki3331007@users.noreply.github.com>
2023-10-28 10:33:04 +0200

    add contact model

---
+++ db/migrate/20231028083027_create_contacts.rb
@@ -0,0 +1,12 @@
+class CreateContacts < ActiveRecord::Migration[6.1]
+  def change
+    create_table :contacts do |t|
+        t.string :name, null: false
+        t.string :email, null: false
+        t.integer :integer, null: false
+        t.string :title, null: false
+        t.string :body, null: false
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_10_28_072721) do
+ActiveRecord::Schema.define(version: 2023_10_28_083027) do
 
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -27,6 +27,16 @@ ActiveRecord::Schema.define(version: 2023_10_28_072721) do
   create_table "comments", force: :cascade do |t|
     t.integer "user_id", null: false
     t.integer "post_id", null: false
+    t.string "body", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "contacts", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "email", null: false
+    t.integer "integer", null: false
+    t.string "title", null: false
     t.string "body", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
