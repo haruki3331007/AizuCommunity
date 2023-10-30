65c8e38920af56b4420fea8e6f3f3017e45786b1
haruki3331007 <116860864+haruki3331007@users.noreply.github.com>
2023-10-30 23:29:38 +0100

    add post edit

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -24,7 +24,7 @@ class PostsController < ApplicationController
 
     def update
         @post = Post.find(params[:id])
-        if post.update(post_params)
+        if @post.update(post_params)
             redirect_to posts_path
         else
             render :edit
