582b18b9035d3c967e17d3649fe77089bab2d158
haruki3331007 <116860864+haruki3331007@users.noreply.github.com>
2023-10-31 20:05:01 +0100

    add contact destroy func.

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -32,7 +32,9 @@ class ContactsController < ApplicationController
     end
 
     def destroy
-
+        contact = Contact.find(params[:id])
+        contact.destroy
+        redirect_to contacts_path
     end
 
     private
